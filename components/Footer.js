5e8a96194c5b31215fbbb6a04eb7b2a0ad1b67a7
Kevin Bellock <kevinbellock@Kevins-MacBook-Air.local>
2021-05-24 07:17:51 -0700

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer className="flex justify-evenly p-5">
+            <h2>Created 2021 | Kevin Bellock</h2>
+            <p>Uses tmdb.org API | Portfolio Example Purposes Only</p>
+        </footer>
+    )
+}
+
+export default Footer
